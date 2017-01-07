24b6a20ce50061ec4b9f700c3b44e9e395938f3f
BrianJConway <BrianC@nevada.unr.edu>
2017-01-06 22:07:07 -0800

    added cactus obstacles

--- Assets/Scripts/BunnyController.cs
+++ Assets/Scripts/BunnyController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BunnyController : MonoBehaviour {
     private Rigidbody2D myRigidBody;
@@ -22,4 +23,12 @@ public class BunnyController : MonoBehaviour {
 
         myAnim.SetFloat( "v_velocity", myRigidBody.velocity.y );
 	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if( collision.transform.tag == "Enemy" )
+        {
+            SceneManager.LoadScene("Game");
+        }
+    }
 }
